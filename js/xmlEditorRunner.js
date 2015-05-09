cb47813e1af0c83d0c8dbfe1bbd71afde588b713
mekanixms <cezar.lucan@gmail.com>
2015-05-09 11:58:53 +0300

    updated README

--- js/xmlEditorRunner.js
+++ js/xmlEditorRunner.js
@@ -1,18 +1,11 @@
-/**
- * @preserve
- * XMS - Online Web Development
- * 
- * Copyright (c) 2010 Cezar Lucan cezar.lucan@aws-dms.com
- * Licensed under GPL license.
- * http://www.aws-dms.com
- *
- * Date: 2010-10-24
- */
-$(document).ready(function()
-{
-	var o = $("#xmlEditorContainer").xmlEditor();
-	$( document ).tooltip();
-});
+/*
 
+ XMS - Online Web Development
 
+ Copyright (c) 2010 Cezar Lucan cezar.lucan@aws-dms.com
+ Licensed under GPL license.
+ http://www.aws-dms.com
 
+ Date: 2010-10-24
+*/
+$(document).ready(function(){$("#xmlEditorContainer").xmlEditor();$(document).tooltip()});
